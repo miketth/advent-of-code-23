b9ebcceeca5c2b6221b297fa4ada9157dc296ace
Miklós Tóth <tothmiklostibor@gmail.com>
2023-12-09 17:44:01 +0100

    Solve day 9, part 2

--- 9_erlang/src/main.erl
+++ 9_erlang/src/main.erl
@@ -9,8 +9,11 @@ start() ->
     NonEmptyLines = lists:filter(fun(Line) -> Line /= "" end, Lines),
     Series = lists:map(fun line_to_nums/1, NonEmptyLines),
     AllNext = lists:map(fun find_next/1, Series),
-    Sum = lists:foldl(fun(X, Acc) -> X + Acc end, 0, AllNext),
-    io:format('First part: ~p~n', [Sum]).
+    SumNext = lists:foldl(fun(X, Acc) -> X + Acc end, 0, AllNext),
+    io:format('First part: ~p~n', [SumNext]),
+    AllPrev = lists:map(fun find_prev/1, Series),
+    SumPrev = lists:foldl(fun(X, Acc) -> X + Acc end, 0, AllPrev),
+    io:format('Second part: ~p~n', [SumPrev]).
 
 to_int(Str) ->
     {Int, _ } = string:to_integer(Str),
@@ -37,4 +40,14 @@ find_next(List) ->
             Last + NextDiff
     end.
 
+find_prev(List) ->
+    Diffs = get_diffs(List),
+    AllZeroes = lists:all(fun(X) -> X == 0 end, Diffs),
+    [First | _] = List,
+    if
+        AllZeroes -> First;
+        true ->
+            PrevDiff = find_prev(Diffs),
+            First - PrevDiff
+    end.
 
