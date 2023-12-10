a38543c542ac573969836140e56ce5f3d11eca3a
Miklós Tóth (Mike) <tothmiklostibor@gmail.com>
2023-12-11 00:39:56 +0100

    Solve day 10, part 2

--- 10_kotlin/src/main/kotlin/Main.kt
+++ 10_kotlin/src/main/kotlin/Main.kt
@@ -2,17 +2,21 @@ import java.io.File
 import java.util.*
 
 fun main(args: Array<String>) {
-    val vertices =
-        readInputFile()
-            .filter { it.isNotEmpty() }
-            .mapIndexed { index, line -> processLine(line, index) }
-            .flatten()
+    val grid = readInputFile()
+        .filter { it.isNotEmpty() }
+        .mapIndexed { index, line -> processLine(line, index) }
+
+    val vertices = grid.flatten().filterNotNull()
 
     val start = vertices.first { it.isStart }
     val graph = processVertices(vertices)
     val distances = graph.bfs(start.coords)
     val maxDistance = distances.maxOfOrNull { it.second }
     println("First part: $maxDistance")
+
+    graph.filterVerticesNotConnectedTo(start.coords)
+    val count = findEnclosedNum(grid, graph)
+    println("Second part: $count")
 }
 
 fun readInputFile(fileName: String = "inputs/input") =
@@ -22,33 +26,36 @@ fun readInputFile(fileName: String = "inputs/input") =
         .lineSequence()
         .toList()
 
-fun processLine(line: String, y: Int): List<PossibleVertex> =
+fun processLine(line: String, y: Int): List<PossibleVertex?> =
     line.mapIndexed { index, c ->
         when (c) {
             '.' -> null
             '|' -> PossibleVertex(
                 coords = Coordinate(index, y),
-                possiblyConnects = listOf(Coordinate(index, y - 1), Coordinate(index, y + 1))
+                possiblyConnects = listOf(Coordinate(index, y - 1), Coordinate(index, y + 1)),
+                isBarrier = true,
             )
             '-' -> PossibleVertex(
                 coords = Coordinate(index, y),
-                possiblyConnects = listOf(Coordinate(index - 1, y), Coordinate(index + 1, y))
+                possiblyConnects = listOf(Coordinate(index - 1, y), Coordinate(index + 1, y)),
             )
             'L' -> PossibleVertex(
                 coords = Coordinate(index, y),
-                possiblyConnects = listOf(Coordinate(index, y - 1), Coordinate(index + 1, y))
+                possiblyConnects = listOf(Coordinate(index, y - 1), Coordinate(index + 1, y)),
             )
             'J' -> PossibleVertex(
                 coords = Coordinate(index, y),
-                possiblyConnects = listOf(Coordinate(index, y - 1), Coordinate(index - 1, y))
+                possiblyConnects = listOf(Coordinate(index, y - 1), Coordinate(index - 1, y)),
             )
             '7' -> PossibleVertex(
                 coords = Coordinate(index, y),
-                possiblyConnects = listOf(Coordinate(index - 1, y), Coordinate(index, y+1))
+                possiblyConnects = listOf(Coordinate(index - 1, y), Coordinate(index, y+1)),
+                isBarrier = true,
             )
             'F' -> PossibleVertex(
                 coords = Coordinate(index, y),
-                possiblyConnects = listOf(Coordinate(index + 1, y), Coordinate(index, y+1))
+                possiblyConnects = listOf(Coordinate(index + 1, y), Coordinate(index, y+1)),
+                isBarrier = true,
             )
             'S' -> PossibleVertex(
                 coords = Coordinate(index, y),
@@ -58,15 +65,23 @@ fun processLine(line: String, y: Int): List<PossibleVertex> =
                     Coordinate(index - 1, y),
                     Coordinate(index + 1, y),
                 ),
-                isStart = true
+                isStart = true,
+                isBarrier = true, // yolo?
             )
             else -> throw Exception("Unexpected character $c")
         }
-    }.filterNotNull()
+    }
 
 data class Coordinate(val x: Int, val y: Int)
 
-data class PossibleVertex(val coords: Coordinate, val possiblyConnects: List<Coordinate>, val isStart: Boolean = false)
+data class PossibleVertex(
+    val coords: Coordinate,
+    val possiblyConnects: List<Coordinate>,
+    val isStart: Boolean = false,
+    val isVerticalBarrier: Boolean = false,
+    val isHorizontalBarrier: Boolean = false,
+    val isBarrier: Boolean = false
+)
 
 class Graph {
     val vertices = mutableSetOf<Coordinate>()
@@ -81,11 +96,12 @@ class Graph {
         }
     }
 
-    fun filterVertices() {
+    fun filterVerticesNotConnectedTo(start: Coordinate) {
         val toRemove = mutableSetOf<Coordinate>()
+        val reachable = bfs(start).map { it.first }
         vertices.forEach { coords ->
-            if (edges[coords]?.isEmpty() != false) {
-                toRemove.add(coords)
+            if (!reachable.contains(coords)) {
+                toRemove += coords
             }
         }
         vertices.removeAll(toRemove)
@@ -114,6 +130,9 @@ class Graph {
 
         return traversalOrder
     }
+
+    fun isPart(coords: Coordinate) = vertices.contains(coords)
+    fun isNeighbor(coords: Coordinate, neighbor: Coordinate) = edges[coords]!!.contains(neighbor) ?: false
 }
 
 fun processVertices(vertices: List<PossibleVertex>): Graph {
@@ -125,8 +144,44 @@ fun processVertices(vertices: List<PossibleVertex>): Graph {
     }
 
     graph.filterEdges()
-    graph.filterVertices()
 
     return graph
 }
 
+fun findEnclosedNum(grid: List<List<PossibleVertex?>>, graph: Graph): Int {
+    var count = 0
+
+    grid.forEachIndexed { y, row ->
+        row.forEachIndexed { x, vertex ->
+            if (isInside(grid, graph, x, y)) {
+                count++
+            }
+        }
+    }
+
+    return count
+}
+
+fun isInside(grid: List<List<PossibleVertex?>>, graph: Graph, x: Int, y: Int): Boolean {
+    if (graph.isPart(Coordinate(x, y))) {
+        return false
+    }
+
+    var cross = 0
+    val line = grid[y]
+    line.forEachIndexed { currX, it ->
+        if (currX > x) {
+            return@forEachIndexed
+        }
+        val isBarrier = it?.let {
+            graph.isPart(it.coords) && it.isBarrier
+        } ?: false
+        if (isBarrier) {
+            cross++
+        }
+    }
+
+    return cross.isOdd()
+}
+
+fun Int.isOdd() = this % 2 == 1
